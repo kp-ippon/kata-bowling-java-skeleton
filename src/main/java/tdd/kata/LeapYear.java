5d70ccd43a98aeed9a5dfe81ef03d377c0935dec
monxxi <prevost.kev@gmail.com>
2023-10-10 09:20:36 +0200

    add Leap Year

---
+++ src/main/java/tdd/kata/LeapYear.java
@@ -0,0 +1,8 @@
+package tdd.kata;
+
+public class LeapYear {
+
+	public static boolean isLeapYear(int year) {
+		throw new IllegalStateException("Not implemented yet");
+	}
+}
---
+++ src/test/java/tdd/kata/LeapYearTest.java
@@ -0,0 +1,13 @@
+package tdd.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LeapYearTest {
+
+	@Test
+	void should_be_true_if_installation_work() {
+		assertThat(true).isTrue();
+	}
+}
